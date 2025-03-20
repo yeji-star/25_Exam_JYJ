b246a182bdc774a7a739c717b256ac4c2162d1c7
yeji-star <jjjsk134@gmail.com>
2025-03-20 15:26:47 +0900

    1번 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// 1. 가로길이, 세로길이를 입력받아 사각형 넓이 만큼 별을 찍어내는 프로그램을 구현
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        int garo = sc.nextInt();
+        int sero = sc.nextInt();
+
+
+
+        for (int i = 1; i <= sero; i++) {
+
+            System.out.print("*");
+
+            for (int j = 1; j < garo; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println("");
         }
+
+
+
+
+
+
     }
 }
