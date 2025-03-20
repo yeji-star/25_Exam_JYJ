dddeadb63d229c8e30f06278c67f187dcdf952fc
yeji-star <jjjsk134@gmail.com>
2025-03-20 16:22:05 +0900

    2번 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
-// 1. 가로길이, 세로길이를 입력받아 사각형 넓이 만큼 별을 찍어내는 프로그램을 구현
+// 2. 양의 정수 n을 요청받아 숫자 패턴의 피라미드를 출력하는 프로그램을 구현합니다.
+// 피라미드는 n 레벨을 가져야 하며, 최상위 레벨에서 n부터 시작하여 맨 아래층에서 1까지 숫자가 감소해야 합니다.
 
 import java.util.Scanner;
 
@@ -8,26 +9,20 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int garo = sc.nextInt();
-        int sero = sc.nextInt();
+        int n = sc.nextInt();
 
+        for (int j = 0; j <= n + 4; j++) {
 
-
-        for (int i = 1; i <= sero; i++) {
-
-            System.out.print("*");
-
-            for (int j = 1; j < garo; j++) {
-
-                System.out.print("*");
+            for (int i = n; i >= 1; i--) {
+                System.out.print(i);
+                System.out.print(" ");
             }
+
             System.out.println("");
+
+            n -= 1;
         }
-
-
-
-
-
-
     }
+
 }
+
